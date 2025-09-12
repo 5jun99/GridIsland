c217aa69a11ad048fa004c9c920ad99b3e21ad0b
5jun99 <oj041088@gmail.com>
2025-09-12 23:23:22 +0000

    set: set spring project

---
+++ src/main/java/com/grid/kwablemap/KWablemapApplication.java
@@ -0,0 +1,13 @@
+package com.grid.kwablemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KWablemapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KWablemapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grid/kwablemap/KWablemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grid.kwablemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KWablemapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
