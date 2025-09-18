6a848dd9adb02c55e228ab570a669dc9981c9f2d
5jun99 <oj041088@gmail.com>
2025-09-18 23:23:22 +0000

    ✨ feat: API 응답 공통 구조 추가#2
    - ApiResponse 제네릭 응답 래퍼 클래스 구현
    - StatusCode enum으로 상태 코드 관리 체계 구축

---
+++ src/main/java/com/grid/kwablemap/api/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.grid.kwablemap.api;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiResponse<T> {
+    private String code;
+    private String message;
+    private T data;
+    private HttpStatus status;
+
+    public ApiResponse(String code, String message, T data, HttpStatus status) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.status = status;
+    }
+
+    public static <T> ApiResponse<T> of(StatusCode statusCode, String message, T data) {
+        return new ApiResponse(statusCode.getCode(), message, data, statusCode.getStatus());
+    }
+}
